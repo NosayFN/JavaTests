79ab2020a92b9bd61d68aa5b45a35d7cb3283810
Kirill Belov <kirbelov05@gmail.com>
2024-05-10 22:32:58 +0300

    First commit

---
+++ src/test/java/MainTests.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTests {
+    @Test
+    public void test_min() {
+        Main.numbers = new int[]{0, 1, 2, 3, 0};
+        Assertions.assertEquals(0, Main._min());
+    }
+
+    @Test
+    public void test_max() {
+        Main.numbers = new int[]{7, 1, 2, 3, 0};
+        Assertions.assertEquals(7, Main._max());
+    }
+
+    @Test
+    public void test_sum() {
+        int[] numbers = new int[]{1, 2, 3, 0};
+        Assertions.assertEquals(6, Main._sum());
+    }
+
+    @Test
+    public void test_mult() {
+        Main.numbers = new int[]{1, 2, 3, 7};
+        Assertions.assertEquals(42, Main._mult());
+    }
+
+    @Test
+    public void test_mult_zero() {
+        Main.numbers = new int[]{1, 2, 3, 0};
+        Assertions.assertEquals(0, Main._mult());
+    }
+}
